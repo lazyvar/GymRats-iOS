8b61de1f2a8ea3f185e03d6449d712a8b6fb0b47
Mack <mack@hasz.email>
2019-05-29 21:25:17 -0700

    now ya done it

--- GymRats/GymRats/feature/view-challenge/ChallengeViewController.swift
+++ GymRats/GymRats/feature/view-challenge/ChallengeViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import Pageboy
+import RxSwift
+import RxCocoa
 
 class ChallengeViewController: UIViewController {
     
+    private let disposeBag = DisposeBag()
+    
     var challenge: Challenge!
-
+    
     @IBOutlet weak var challengeImageView: UserImageView! {
         didSet {
             challengeImageView.load(avatarInfo: challenge)
@@ -41,12 +46,31 @@ class ChallengeViewController: UIViewController {
         }
     }
     
+    lazy var pageboyViewController: PageboyViewController = {
+        let pageboyViewController = PageboyViewController()
+        pageboyViewController.dataSource = self
+        pageboyViewController.delegate = self
+
+        return pageboyViewController
+    }()
+    
+    @IBOutlet weak var sliderContainer: UIView! {
+        didSet {
+            addChild(pageboyViewController)
+            sliderContainer.addSubview(pageboyViewController.view)
+            pageboyViewController.didMove(toParent: self)
+        }
+    }
+    
     lazy var chatItem = UIBarButtonItem (
         image: UIImage(named: "chat")?.withRenderingMode(.alwaysOriginal),
         style: .plain,
         target: self,
         action: #selector(openChat)
     )
+
+    var users: [User] = []
+    var workouts: [Workout] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,10 +81,102 @@ class ChallengeViewController: UIViewController {
         title = "💪"
         
         navigationItem.rightBarButtonItem = chatItem
+        fetchUserWorkouts()
     }
     
     @objc func openChat() {
         push(ChatViewController(challenge: challenge))
+    }
+
+    @objc func fetchUserWorkouts() {
+        let users = gymRatsAPI.getUsers(for: challenge)
+        let workouts = gymRatsAPI.getWorkouts(for: challenge)
+        
+        showLoadingBar()
+        
+        Observable.zip(users, workouts).subscribe(onNext: { zipped in
+            let (users, workouts) = zipped
+            
+            self.users = users
+            self.workouts = workouts
+            
+            self.hideLoadingBar()
+            self.pageboyViewController.reloadData()
+        }, onError: { error in
+            // TODO
+            self.hideLoadingBar()
+        }).disposed(by: disposeBag)
+    }
+
+    func userWorkouts(for date: Date) -> [UserWorkout] {
+        let workoutsForToday = self.workouts.workouts(on: date)
+        
+        return users.flatMap({ (user: User) -> [UserWorkout] in
+            let workouts = workoutsForToday.filter { $0.gymRatsUserId == user.id }
+            
+            if workouts.isEmpty {
+                return [UserWorkout(user: user, workout: nil)]
+            } else {
+                return workouts.map { UserWorkout(user: user, workout: $0) }
+            }
+        }).sorted(by: { a, b in
+            if let aWorkout = a.workout, let bWorkout = b.workout {
+                return aWorkout.createdAt > bWorkout.createdAt
+            } else if a.workout != nil {
+                return true
+            } else if b.workout != nil  {
+                return false
+            } else {
+                return a.user.fullName < b.user.fullName
+            }
+        })
+    }
+
+}
+
+extension ChallengeViewController: PageboyViewControllerDelegate {
+    
+    func pageboyViewController(_ pageboyViewController: PageboyViewController, willScrollToPageAt index: PageboyViewController.PageIndex, direction: PageboyViewController.NavigationDirection, animated: Bool) {
+        
+    }
+    
+    func pageboyViewController(_ pageboyViewController: PageboyViewController, didScrollTo position: CGPoint, direction: PageboyViewController.NavigationDirection, animated: Bool) {
+        
+    }
+    
+    func pageboyViewController(_ pageboyViewController: PageboyViewController, didScrollToPageAt index: PageboyViewController.PageIndex, direction: PageboyViewController.NavigationDirection, animated: Bool) {
+
+        guard let vc = pageboyViewController.currentViewController as? ChallengeDayViewController else { return }
+        
+        vc.loadData()
+    }
+    
+    func pageboyViewController(_ pageboyViewController: PageboyViewController, didReloadWith currentViewController: UIViewController, currentPageIndex: PageboyViewController.PageIndex) {
+        print("WHAT")
+    }
+    
+    
+}
+
+extension ChallengeViewController: PageboyViewControllerDataSource {
+    
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        return challenge.days.count
+    }
+    
+    func viewController (
+        for pageboyViewController: PageboyViewController,
+        at index: PageboyViewController.PageIndex
+    ) -> UIViewController? {
+        let date = challenge.days[index]
+        let userWorkouts = self.userWorkouts(for: date)
+        let challengeDayViewController = ChallengeDayViewController(date: date, userWorkouts: userWorkouts, challenge: challenge)
+        
+        return challengeDayViewController
+    }
+    
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        return .last
     }
 
 }
