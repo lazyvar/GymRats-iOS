65a78e315d33bfa5784c63877d6ebc017d0aa1f4
Mack <mack@hasz.email>
2019-07-07 16:03:35 -0700

    Correct layout for workout view

--- GymRats/GymRats/feature/workout/WorkoutViewController.swift
+++ GymRats/GymRats/feature/workout/WorkoutViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 import RxSwift
-import YogaKit
 import MapKit
 import SkeletonView
+import Kingfisher
 
 class WorkoutViewController: UITableViewController {
 
@@ -43,6 +43,8 @@ class WorkoutViewController: UITableViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    weak var headerView: WorkoutHeaderView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -55,166 +57,13 @@ class WorkoutViewController: UITableViewController {
 
         setupBackButton()
         
-        let containerView = UIView()
-        containerView.frame = view.frame
-        containerView.backgroundColor = .white
+        self.headerView = WorkoutHeaderView.instanceFromNib()
+        headerView.configure(user: user, workout: workout, challenge: challenge, width: tableView.frame.width)
+        headerView.delegate = self
+        headerView.translatesAutoresizingMaskIntoConstraints = false
         
-        let headerView = UIView()
-        
-        let userImageView = UserImageView()
-        userImageView.load(avatarInfo: user)
-        
-        let usernameLabel: UILabel = UILabel()
-        usernameLabel.font = .body
-        usernameLabel.text = user.fullName
-
-        let timeLabel: UILabel = UILabel()
-        timeLabel.font = .details
-        timeLabel.text = workout.createdAt.challengeTime
-        timeLabel.textAlignment = .right
-        
-        headerView.configureLayout { layout in
-            layout.isEnabled = true
-            layout.flexDirection = .row
-            layout.justifyContent = .flexStart
-            layout.padding = 10
-        }
-        
-        userImageView.configureLayout { layout in
-            layout.isEnabled = true
-            layout.width = 32
-            layout.height = 32
-        }
-        
-        usernameLabel.configureLayout { layout in
-            layout.isEnabled = true
-            layout.marginLeft = 10
-        }
-
-        timeLabel.configureLayout { layout in
-            layout.isEnabled = true
-            layout.flexGrow = 1
-        }
-
-        headerView.addSubview(userImageView)
-        headerView.addSubview(usernameLabel)
-        headerView.addSubview(timeLabel)
-
-        headerView.yoga.applyLayout(preservingOrigin: true)
-        
-        let tap = UITapGestureRecognizer(target: self, action: #selector(transitionToProfile))
-        tap.numberOfTapsRequired = 1
-        
-        headerView.isUserInteractionEnabled = true
-        headerView.addGestureRecognizer(tap)
-        
-        containerView.configureLayout { layout in
-            layout.isEnabled = true
-            layout.flexDirection = .column
-            layout.justifyContent = .flexStart
-        }
-        
-        if let pictureUrl = workout.photoUrl, let url = URL(string: pictureUrl) {
-            let imageView = UIImageView()
-            imageView.contentMode = .scaleAspectFill
-            imageView.clipsToBounds = true
-            imageView.backgroundColor = .whiteSmoke
-
-            imageView.configureLayout { layout in
-                layout.isEnabled = true
-                layout.width = YGValue(self.view.frame.width)
-                layout.aspectRatio = 1
-            }
-
-            imageView.kf.setImage(with: url) { image, _, _, _ in
-                guard let image = image else { return }
-                
-                let width = image.size.width
-                let height = image.size.height
-                
-                imageView.configureLayout { layout in
-                    layout.isEnabled = true
-                    layout.aspectRatio = width / height
-                }
-                containerView.yoga.applyLayout(preservingOrigin: true)
-            }
-            containerView.addSubview(imageView)
-        }
-        
-        if let placeId = workout.googlePlaceId {
-            GService.getPlaceInformation(forPlaceId: placeId)
-                .subscribe { event in
-                    switch event {
-                    case .next(let place):
-                        let mapView = MKMapView()
-                        let initialLocation = CLLocation(latitude: place.latitude, longitude: place.longitude)
-                        let coordinateRegion = MKCoordinateRegion (
-                            center: initialLocation.coordinate,
-                            latitudinalMeters: 500, longitudinalMeters: 500
-                        )
-                        let annotation = PlaceAnnotation (
-                            title: place.name,
-                            coordinate: CLLocationCoordinate2D (
-                                latitude: place.latitude,
-                                longitude: place.longitude
-                            )
-                        )
-                        
-                        mapView.setRegion(coordinateRegion, animated: true)
-                        mapView.mapType = .standard
-                        mapView.isUserInteractionEnabled = false
-                        mapView.addAnnotation(annotation)
-                        
-                        mapView.configureLayout { layout in
-                            layout.isEnabled = true
-                            layout.width = YGValue(self.view.frame.width)
-                            layout.height = 115
-                            layout.marginTop = 2
-                        }
-                        DispatchQueue.main.async { [weak self] in
-                            if self?.workout.photoUrl == nil {
-                                containerView.insertSubview(mapView, at: 0)
-                            } else {
-                                containerView.insertSubview(mapView, at: 1)
-                            }
-                            containerView.yoga.applyLayout(preservingOrigin: true, dimensionFlexibility: .flexibleHeight)
-                        }
-                    default: break
-                    }
-                }.disposed(by: disposeBag)
-        }
-        
-        containerView.addSubview(headerView)
-        
-        let titleLabel: UILabel = UILabel()
-        titleLabel.font = .body
-        titleLabel.text = workout.title
-        
-        titleLabel.configureLayout { layout in
-            layout.isEnabled = true
-            layout.margin = 10
-            layout.marginTop = 5
-        }
-        
-        containerView.addSubview(titleLabel)
-        
-        if let description = workout.description {
-            let descriptionLabel = UILabel()
-            descriptionLabel.text = description
-            descriptionLabel.numberOfLines = 0
-            descriptionLabel.font = .body
-            descriptionLabel.configureLayout { layout in
-                layout.isEnabled = true
-                layout.margin = 10
-                layout.marginTop = 0
-            }
-            
-            containerView.addSubview(descriptionLabel)
-        }
-
-        containerView.yoga.applyLayout(preservingOrigin: true)
-
-        tableView.tableHeaderView = containerView
+        tableView.showsVerticalScrollIndicator = false
+        tableView.tableHeaderView = headerView
         tableView.separatorStyle = .none
         tableView.addSubview(refresher)
         tableView.register(UINib(nibName: "CommentTableViewCell", bundle: nil), forCellReuseIdentifier: "CommentTableViewCell")
@@ -224,7 +73,7 @@ class WorkoutViewController: UITableViewController {
         fetchComments()
         
         let tapToHideKeyboard = UITapGestureRecognizer()
-        tap.numberOfTapsRequired = 1
+        tapToHideKeyboard.numberOfTapsRequired = 1
         tapToHideKeyboard.addTarget(self, action: #selector(hideKeyboard))
         
         view.addGestureRecognizer(tapToHideKeyboard)
@@ -330,6 +179,17 @@ class WorkoutViewController: UITableViewController {
         self.present(alertViewController, animated: true, completion: nil)
     }
     
+}
+
+extension WorkoutViewController: WorkoutHeaderViewDelegate {
+    func tappedHeader() {
+        self.transitionToProfile()
+    }
+    func layoutTableView() {
+        self.tableView.setNeedsLayout()
+        self.tableView.layoutIfNeeded()
+        self.tableView.reloadData()
+    }
 }
 
 extension WorkoutViewController {
