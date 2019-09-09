83c082a7bfe774450d8c9c232e40ebb2b062350b
Mack <mack@hasz.email>
2019-09-08 17:48:33 -0700

    leaders

---
+++ GymRats/GymRats/feature/view-challenge/HumanCell.swift
@@ -0,0 +1,21 @@
+//
+//  HumanCell.swift
+//  GymRats
+//
+//  Created by Mack on 9/8/19.
+//  Copyright © 2019 Mack Hasz. All rights reserved.
+//
+
+import UIKit
+
+class HumanCell: UICollectionViewCell {
+
+    @IBOutlet weak var userImageView: UserImageView!
+    @IBOutlet weak var humanLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
