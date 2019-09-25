c5d4def1c302a0324f6bb9b6cabf71988af05087
Mack <mack@hasz.email>
2019-09-24 19:55:40 -0700

    prod api

--- GymRats/GymRats/service/API.swift
+++ GymRats/GymRats/service/API.swift
@@ -167,7 +167,7 @@ class GymRatsAPI {
     
     private let networkProvider: NetworkProvider
     
-    init(networkProvider: NetworkProvider = DevelopmentNetworkProvider()) {
+    init(networkProvider: NetworkProvider = ProductionNetworkProvider()) {
         self.networkProvider = networkProvider
     }
     
