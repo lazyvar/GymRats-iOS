60dfa21e724149ae7a22dd742733cba5598276af
Mack Hasz <mack@hasz.email>
2020-05-06 10:12:35 -0700

    ask every 60 days

--- GymRats/service/StoreService.swift
+++ GymRats/service/StoreService.swift
@@ -15,11 +15,14 @@ class StoreService {
     count += 1
     UserDefaults.standard.set(count, forKey: UserDefaultsKeys.processCompletedCountKey)
     
-    if count >= 8 && !UserDefaults.standard.bool(forKey: UserDefaultsKeys.requestReview) {
-      DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+    let lastAskJulianDay = UserDefaults.standard.integer(forKey: UserDefaultsKeys.lastAskJulianDay)
+    let currentJulianDay = Int(Date().julianDay)
+    
+    if count >= 8, currentJulianDay > (lastAskJulianDay + 60) {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
         SKStoreReviewController.requestReview()
         Track.event(.storeReviewRequested)
-        UserDefaults.standard.set(true, forKey: UserDefaultsKeys.requestReview)
+        UserDefaults.standard.set(currentJulianDay, forKey: UserDefaultsKeys.lastAskJulianDay)
       }
     }
   }
@@ -27,5 +30,5 @@ class StoreService {
 
 class UserDefaultsKeys {
   class var processCompletedCountKey: String { return "processCompletedCount" }
-  class var requestReview: String { return "requestReview" }
+  class var lastAskJulianDay: String { return "lastAskJulianDay" }
 }
