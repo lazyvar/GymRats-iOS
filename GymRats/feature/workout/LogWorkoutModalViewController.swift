cc31adb8c276a92f2903a8bdbca0f9061f95f8d9
Mack Hasz <mack@hasz.email>
2020-10-07 08:54:41 -0700

    fix workout modal

--- GymRats/feature/workout/LogWorkoutModalViewController.swift
+++ GymRats/feature/workout/LogWorkoutModalViewController.swift
@@ -9,101 +9,166 @@
 import UIKit
 import PanModal
 
-class LogWorkoutModalViewController: UITableViewController {
+class LogWorkoutModalViewController: UIViewController, UINavigationControllerDelegate {
+  var showText = true
+  
+  private let onPickImage: (UIImage) -> Void
+  
+  @IBOutlet private weak var chooseFromLibraryView: UIView!
+  @IBOutlet private weak var takePictureView: UIView!
+  @IBOutlet private weak var libraryImageView: UIImageView!
+  @IBOutlet private weak var cameraImageView: UIImageView!
+  @IBOutlet private weak var textStackView: UIStackView!
 
-    let onPickImage: (UIImage) -> Void
-    
-    var showText = true
-    
-    init(onPickImage: @escaping (UIImage) -> Void) {
-        self.onPickImage = onPickImage
-        
-        super.init(style: .plain)
+  let shortPressLibrary: UILongPressGestureRecognizer = {
+      let press = UILongPressGestureRecognizer()
+      press.minimumPressDuration = 0.05
+      press.cancelsTouchesInView = false
+      
+      return press
+  }()
+  
+  let shortPressPicture: UILongPressGestureRecognizer = {
+      let press = UILongPressGestureRecognizer()
+      press.minimumPressDuration = 0.05
+      press.cancelsTouchesInView = false
+      
+      return press
+  }()
+
+  @objc func handleLibrary(shortPress: UILongPressGestureRecognizer) {
+    switch shortPress.state {
+    case .began:
+      animateScaleLibrary(0.925)
+    case .ended, .failed:
+      self.presentLibrary()
+      fallthrough
+    case .cancelled:
+      animateScaleLibrary(1.0)
+    default:
+      break
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+  }
+
+  @objc func handlePicture(shortPress: UILongPressGestureRecognizer) {
+    switch shortPress.state {
+    case .began:
+      animateScalePicture(0.925)
+    case .ended, .failed:
+      self.takePicture()
+      fallthrough
+    case .cancelled:
+      animateScalePicture(1.0)
+    default:
+      break
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  }
 
-        tableView.backgroundColor = .background
-        tableView.separatorStyle = .none
-        tableView.allowsSelection = false
-        tableView.register(UINib(nibName: "LogWorkoutCell", bundle: nil), forCellReuseIdentifier: "log")
+  init(onPickImage: @escaping (UIImage) -> Void) {
+    self.onPickImage = onPickImage
+      
+    super.init(nibName: Self.xibName, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func viewDidLoad() {
+    view.backgroundColor = .background
+
+    shortPressLibrary.addTarget(self, action: #selector(handleLibrary(shortPress:)))
+    shortPressLibrary.delegate = self
+    shortPressPicture.addTarget(self, action: #selector(handlePicture(shortPress:)))
+    shortPressPicture.delegate = self
+
+    chooseFromLibraryView.layer.cornerRadius = 4
+    takePictureView.layer.cornerRadius = 4
+    
+    chooseFromLibraryView.backgroundColor = .foreground
+    takePictureView.backgroundColor = .foreground
+    
+    chooseFromLibraryView.addGestureRecognizer(shortPressLibrary)
+    takePictureView.addGestureRecognizer(shortPressPicture)
+    
+    textStackView.isHidden = !showText
+  }
+  
+  func animateScaleLibrary(_ scale: CGFloat, onCompletion: (() -> Void)? = nil) {
+    UIView.animate(withDuration: 0.15, delay: 0, options: .curveLinear, animations: {
+      self.chooseFromLibraryView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }, completion: { _ in
+        onCompletion?()
+    })
+  }
+
+  func animateScalePicture(_ scale: CGFloat, onCompletion: (() -> Void)? = nil) {
+    UIView.animate(withDuration: 0.15, delay: 0, options: .curveLinear, animations: {
+      self.takePictureView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }, completion: { _ in
+        onCompletion?()
+    })
+  }
+
+  private func takePicture() {
+    guard UIImagePickerController.isCameraDeviceAvailable(.front) || UIImagePickerController.isCameraDeviceAvailable(.rear) else {
+      self.presentAlert(title: "Uh-oh", message: "Your device needs a camera to do that.")
+      return
     }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int { return 1 }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return 1 }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "log") as! LogWorkoutCell
-        cell.textStackView.isHidden = !showText
-        cell.onTakePicture = { [weak self] in
-            guard let self = self else { return }
-            
-            guard UIImagePickerController.isCameraDeviceAvailable(.front) || UIImagePickerController.isCameraDeviceAvailable(.rear) else {
-                self.presentAlert(title: "Uh-oh", message: "Your device needs a camera to do that.")
-                return
-            }
 
-                let imagePicker = UIImagePickerController()
-                imagePicker.sourceType = .camera
-                imagePicker.delegate = self
-                UINavigationBar.appearance().isTranslucent = false
-                UINavigationBar.appearance().barTintColor = .background
-                UINavigationBar.appearance().tintColor = .primaryText
-                
-                self.present(imagePicker, animated: true, completion: nil)
-            
-        }
-        cell.onChooseFromLibrary = { [weak self] in
-            guard let self = self else { return }
-            
-                let imagePicker = UIImagePickerController()
-                imagePicker.sourceType = .photoLibrary
-                imagePicker.delegate = self
-                UINavigationBar.appearance().isTranslucent = false
-                UINavigationBar.appearance().barTintColor = .background
-                UINavigationBar.appearance().tintColor = .primaryText
+    let imagePicker = UIImagePickerController()
+    imagePicker.sourceType = .camera
+    imagePicker.delegate = self
+    UINavigationBar.appearance().isTranslucent = false
+    UINavigationBar.appearance().barTintColor = .background
+    UINavigationBar.appearance().tintColor = .primaryText
 
-                self.present(imagePicker, animated: true, completion: nil)
-            
-        }
+    self.present(imagePicker, animated: true, completion: nil)
+  }
+  
+  private func presentLibrary() {
+    let imagePicker = UIImagePickerController()
+    imagePicker.sourceType = .photoLibrary
+    imagePicker.delegate = self
+    UINavigationBar.appearance().isTranslucent = false
+    UINavigationBar.appearance().barTintColor = .background
+    UINavigationBar.appearance().tintColor = .primaryText
 
-        return cell
-    }
-
+    self.present(imagePicker, animated: true, completion: nil)
+  }
 }
 
-extension LogWorkoutModalViewController: UINavigationControllerDelegate {
-
+extension LogWorkoutModalViewController: UIGestureRecognizerDelegate {
+  func gestureRecognizer(
+    _ gestureRecognizer: UIGestureRecognizer,
+    shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+  ) -> Bool {
+    return true
+  }
 }
 
 extension LogWorkoutModalViewController: UIImagePickerControllerDelegate {
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        picker.dismiss(animated: true) {
-            self.dismiss(animated: true) {
-                if let img = info[.originalImage] as? UIImage {
-                    self.onPickImage(img)
-                }
-            }
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    picker.dismiss(animated: true) {
+      self.dismiss(animated: true) {
+        if let img = info[.originalImage] as? UIImage {
+          self.onPickImage(img)
         }
+      }
     }
-    
+  }
 }
 
 extension LogWorkoutModalViewController: PanModalPresentable {
-    
-    var panScrollable: UIScrollView? {
-        return tableView
-    }
-    
-    var showDragIndicator: Bool {
-        return false
-    }
-    
-    
+  var panScrollable: UIScrollView? {
+    return nil
+  }
+
+  var showDragIndicator: Bool {
+    return false
+  }
+  
+  var shortFormHeight: PanModalHeight {
+    return showText ? .contentHeight(191) : .contentHeight(135)
+  }
 }
