fc9b9d5371692be8ce8712f99f4bc20051b70751
Mack Hasz <mack@hasz.email>
2020-12-16 06:48:21 -0800

    avaudiosession for playback and mixWithOthers

--- GymRats/feature/media/VideoViewController.swift
+++ GymRats/feature/media/VideoViewController.swift
@@ -146,6 +146,14 @@ class VideoViewController: UIViewController {
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     
+    let audioSession = AVAudioSession.sharedInstance()
+    
+    do {
+      try audioSession.setCategory(.playback, mode: .moviePlayback, options: [.mixWithOthers])
+    } catch {
+      print("Failed to set audio session category.")
+    }
+    
     playFromBeginning()
     
     NotificationCenter.default.addObserver (
